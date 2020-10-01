85a5504679b1bb5d3aa52e0d85ef2219c6e3ec4e
Cristian <cristian.jaramillo.rojas@gmail.com>
2020-10-01 12:21:51 -0300

    cambio de server

--- server/config/config.js
+++ server/config/config.js
@@ -20,10 +20,10 @@ let dbname = "bd_cafe";
 let username = "cjaramillo";
 let password = "xTgKHMjXat6dev9j";
 
-if (process.env.NODE_ENV === 'DEV') {
-    urlDB = 'mongodb://localhost:27017/' + dbname;
-} else {
-    urlDB = 'mongodb://cjaramillo:xTgKHMjXat6dev9j@cluster0-shard-00-00.su2lq.mongodb.net:27017,cluster0-shard-00-01.su2lq.mongodb.net:27017,cluster0-shard-00-02.su2lq.mongodb.net:27017/db_cafe?ssl=true&replicaSet=atlas-10qqg5-shard-0&authSource=admin&retryWrites=true&w=majority';
-}
+//if (process.env.NODE_ENV === 'DEV') {
+//    urlDB = 'mongodb://localhost:27017/' + dbname;
+//} else {
+urlDB = 'mongodb://cjaramillo:xTgKHMjXat6dev9j@cluster0-shard-00-00.su2lq.mongodb.net:27017,cluster0-shard-00-01.su2lq.mongodb.net:27017,cluster0-shard-00-02.su2lq.mongodb.net:27017/db_cafe?ssl=true&replicaSet=atlas-10qqg5-shard-0&authSource=admin&retryWrites=true&w=majority';
+//}
 
 process.env.URL_DB = urlDB;
